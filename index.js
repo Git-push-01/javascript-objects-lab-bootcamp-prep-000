5dceb35f17c6efca37378109aefa135a13ab654f
Hugo Escandon <hescandon6@gmail.com>
2018-06-25 18:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  object[key] = value
  return object
  }
+ var newObj = Object.assign({}, obj);
+function deleteFromObjectByKey(object, key){
+  delete newObj[key];
+  return newObj
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
